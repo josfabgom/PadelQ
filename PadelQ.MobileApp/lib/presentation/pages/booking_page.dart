f29cf1847f5111b7d328f279634830b21ecd743f
josfabgom <josfabgom@gmail.com>
2026-04-04 08:18:20 -0300

    Fix: v3.1 MOBILE - BookingPage Missing Import

--- PadelQ.MobileApp/lib/presentation/pages/booking_page.dart
+++ PadelQ.MobileApp/lib/presentation/pages/booking_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
 import 'package:lucide_icons/lucide_icons.dart';
+import 'package:go_router/go_router.dart';
 import '../../data/services/booking_service.dart';
 import '../../config/api_config.dart';
 
